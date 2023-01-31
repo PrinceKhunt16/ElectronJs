95c8015c29ad34018c87f60c0ef5646fc33fd7ad
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2023-01-31 10:38:35 +0530

    Browser window property

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const { app, BrowserWindow } = require('electron')
 function createWindow(){
   const win = new BrowserWindow({
     width: 900,
-    height: 600
+    height: 600,
+    frame: true,
+    backgroundColor: '#f2f2f2',
+    resizable: true,
+    title: 'ElectronJs',
+    alwaysOnTop: false
   })
 
   win.loadFile('index.html')
